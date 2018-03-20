20d852ec04c26339ec8b179f22342b1c6ab82f98
joshgressman <joshgressman@gmail.com>
2018-03-20 05:42:35 -0500

    added category filter

--- Todone/Controllers/TodoListViewController.swift
+++ Todone/Controllers/TodoListViewController.swift
@@ -91,6 +91,7 @@ class TodoListViewController: UITableViewController {
             let newItem = Item(context: self.context)
             newItem.title = textField.text!
             newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             
             self.saveItems()
@@ -121,8 +122,16 @@ class TodoListViewController: UITableViewController {
     }
     
     //Get from core data with defualt value of Item.fetchRequest() all items
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest() ) {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate:NSPredicate? = nil ) {
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
         
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
+        }
+        
+
         do {
          itemArray = try context.fetch(request)
         } catch {
@@ -146,11 +155,11 @@ extension TodoListViewController: UISearchBarDelegate {
         //request to view data in coreData
         let request : NSFetchRequest<Item> = Item.fetchRequest()
         
-        request.predicate = NSPredicate(format: "title CONTAINS [cd] %@", searchBar.text!)
+        let predicate = NSPredicate(format: "title CONTAINS [cd] %@", searchBar.text!)
         
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         
-        loadItems(with: request)
+        loadItems(with: request, predicate: predicate)
         
     }
     
