ebea2d191b972813bc5ae4889082b35bd9bccc23
joshgressman <joshgressman@gmail.com>
2018-04-02 18:37:20 -0500

    added swipe controller super class

---
+++ Todone/Controllers/SwipeTableViewController.swift
@@ -0,0 +1,67 @@
+//
+//  SwipeTableViewController.swift
+//  Todone
+//
+//  Created by Josh Gressman on 4/2/18.
+//  Copyright © 2018 Josh Gressman. All rights reserved.
+//
+
+import UIKit
+import SwipeCellKit
+
+
+class SwipeTableViewController: UITableViewController, SwipeTableViewCellDelegate {
+  
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    //Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SwipeTableViewCell
+        cell.delegate = self
+        return cell
+        
+    }
+    
+
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        guard orientation == .right else { return nil }
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            // handle action by updating model with deletion
+            
+            print("Delete cell")
+            
+//            if let categoryForDeletion = self.categories?[indexPath.row] {
+//                do {
+//                    try self.realm.write {
+//                        self.realm.delete(categoryForDeletion)
+//                    }
+//                } catch {
+//                    print("Error deleting category")
+//                }
+//
+//            }
+        }
+        
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "delete-icon")
+        
+        return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions {
+        var options = SwipeTableOptions()
+        options.expansionStyle = .destructive
+        return options
+    }
+    
+
+}
+
+
